73d797559225bc475b3f3ca3b3d6ba62ff3489bc
cvcv789789 <cvcv789789@gmail.com>
2022-05-20 15:00:24 +0800

    0520

--- myweb/myapp/views.py
+++ myweb/myapp/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def hi(request):
     return HttpResponse('<p>13232252222222222222222222222222222223</p>')
 # Create your views here.
+def hi2(request,username):
+    return render(request,'hello3.html',{'username':username})
+def main(request):
+    return HttpResponse('<h1>123213</h1>')
--- myweb/myweb/urls.py
+++ myweb/myweb/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp.views import hi
+from myapp.views import main
+from myapp.views import hi2
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sayhello/', hi),
+    path('sayhello/<str:username>', hi2),
+    path('', main),
 ]
