97112340373b27733ab981d3c31605b6142d1dd2
cvcv789789 <cvcv789789@gmail.com>
2022-05-20 15:54:21 +0800

    0520

--- myweb/myapp/views.py
+++ myweb/myapp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 def hi(request):
     return HttpResponse('<p>13232252222222222222222222222222222223</p>')
 # Create your views here.
 def hi2(request,username):
-    return render(request,'hello3.html',{'username':username})
+    today = datetime.date.today()
+   
+    return render(request,'hello3.html',{'username':username,'now':today})
 def main(request):
     return HttpResponse('<h1>123213</h1>')
